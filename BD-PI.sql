5c5c2393d403f4ca2e2d6e97587137d16618bdec
gablimasilva <gabriel.lsilva@bandtec.com.br>
2021-11-04 19:21:41 -0300

    Primeira tentativa BD/Modelagem

---
+++ BD-PI.sql
@@ -0,0 +1,17 @@
+create database ProjetoIndividual;
+use ProjetoIndividual;
+
+create table usuarios(
+apelido varchar(50) primary key,
+senha varchar(50),
+cpf char(11),
+email varchar(50),
+nome varchar(50)
+);
+
+create table registro(
+idRegistro varchar(50),
+fkApelido varchar(50),
+primary key(idRegistro,fkApelido),
+dataHora DATETIME
+);
