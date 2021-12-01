8d50f7d9df8136a494bc794cc43cf20c8b2a1db7
gablimasilva <gabriel.lsilva@bandtec.com.br>
2021-12-01 20:17:07 -0300

    Projeto quase finalizado

--- Banco de dados/BD-PI.sql
+++ Banco de dados/BD-PI.sql
@@ -1,10 +1,10 @@
 create database ProjetoIndividual;
 use ProjetoIndividual;
+drop database ProjetoIndividual;
 
 create table usuarios(
-idUsuario int primary key,
+email varchar(50) primary key,
 nome varchar(50),
-email varchar(50),
 esporte varchar(50),
 senha varchar(50)
 );
@@ -12,9 +12,30 @@ senha varchar(50)
 select * from usuarios;
 
 create table registro(
-idRegistro int,
-fkUsuario int,
-foreign key (fkUsuario) references usuarios(idUsuario),
-primary key(idRegistro,fkUsuario),
+idRegistro int primary key auto_increment,
+emailUsuario varchar(50),
+foreign key (emailUsuario) references usuarios(email),
 dataHora DATETIME
 );
+
+select * from registro;
+
+SELECT 
+	count(email) as usuario,
+	count(idRegistro) as registros
+		FROM usuarios
+			JOIN registro
+				ON email = emailUsuario;
+
+SELECT count(usuarios.email) as usuario FROM usuarios;
+
+SELECT 
+    count(idRegistro) as registros
+        FROM registro;
+        
+SELECT 
+        count(email) as usuario,
+        count(idRegistro) as registros
+            FROM usuarios
+				JOIN registro
+                    ON email = emailUsuario;
